d4718080742a6f1d16d9407e5bc267d03b9b3f18
Divesh Kumar <diveshkumar9209@outlook.com>
2025-12-04 22:20:25 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,19 @@
 // firebase-config.js
 // Replace the values inside firebaseConfig with your Firebase project values
 export const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID"
-  };
-  
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
-  
-  const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app);
-  export const db = getFirestore(app);
-  
+  apiKey: "AIzaSyAJnlrtazUWr_HjQiEkFNcbnsve73g1pEQ",
+  authDomain: "smart-opd-69488.firebaseapp.com",
+  projectId: "smart-opd-69488",
+  storageBucket: "smart-opd-69488.firebasestorage.app",
+  messagingSenderId: "373276983810",
+  appId: "1:373276983810:web:a4b8808e566d81150d1d1b"
+  measurementId: "G-PN4RQWNCS9"
+};
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
